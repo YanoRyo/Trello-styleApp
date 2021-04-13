ecef8b8ad6c623aad64700dd08035fd6464d2ef3
YanoRyo <yanoryo7032@gmail.com>
2021-04-13 15:10:34 +0900

    userテーブルにnameカラムの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # この1行を追加
+  validates :name, presence: true, length: { maximum: 20 }
 end
