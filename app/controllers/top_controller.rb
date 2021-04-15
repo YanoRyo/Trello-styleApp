0949f744f9994627e1942bc102b8ba93fc52dd4e
YanoRyo <yanoryo7032@gmail.com>
2021-04-15 15:19:19 +0900

    リスト作成機能の実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
 # ==========ここから追加する==========
     def index
+        @lists = List.where(user: current_user).order("created_at ASC")
     end
 # ==========ここまで追加する==========
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #この行を追加
+  has_many :lists, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   # この1行を追加
   validates :name, presence: true, length: { maximum: 20 }
 end
